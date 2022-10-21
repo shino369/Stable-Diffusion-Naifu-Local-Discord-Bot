2f0834a03ac96a00d8cc5fb80e37aa526aa3376b
Anthony Wong <nakagame02@gmail.com>
2022-10-22 06:03:04 +0900

    add config.js

---
+++ src/config.js
@@ -0,0 +1,37 @@
+export const config = {
+  baseUrl: 'http://localhost:6969',
+  default: {
+    portrait: {
+      mid: {
+        height: 768,
+        width: 512,
+      },
+      large: {
+        height: 1024,
+        width: 512,
+      },
+    },
+    landscape: {
+      mid: {
+        height: 512,
+        width: 768,
+      },
+      large: {
+        height: 512,
+        width: 1024,
+      },
+    },
+    square: {
+      mid: {
+        height: 512,
+        width: 512,
+      },
+      large: {
+        height: 1024,
+        width: 1024,
+      },
+    },
+  },
+  negative:
+    'lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, (((ugly))), (((duplicate))), (((morbid))), (((mutilated))), (((tranny))), (trans), (hermaphrodite), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), bad anatomy, bad hands, text, missing fingers, fused fingers, one hand with more than 5 fingers, one hand with less than 5 fingers, signature, watermark, username, multiple breasts, (long body :1.3), black-white, bad anatomy, malformed, mutated, malformed hands, long neck, blurred, lowers, lowres, bad anatomy, uncoordinated body, unnatural body, fused breasts, bad breasts, huge breasts, poorly drawn breasts, extra breasts, more than 2 nipples, missing nipples, bad eyes, fused eyes, poorly drawn eyes, bad shoes, fused shoes, more than two shoes, poorly drawn shoes, malformed feet,',
+}
